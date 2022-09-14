03108587ccf0efa26e49bf0c44e4cce1293493bd
ricky-deriv <ricky.thain@deriv.com>
2022-09-14 16:34:47 +0800

    add variable to contain profile name for aws cli

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -2,12 +2,14 @@
 
 # purpose: to configure aws cli to cloud sandbox aws
 
-echo "creating / overwriting aws profile for sandbox_user"
 
 KEY_ID=$1
 KEY=$2
 REGION_NAME=$3
 OUTPUT_FORMAT=$4
+PROFILE_NAME="sandbox_user"
+
+echo "creating / overwriting aws profile for $PROFILE_NAME"
 
 if [[ (-z "$KEY_ID") || (-z "$KEY") ]]
 then 
@@ -28,13 +30,13 @@ then
     echo "No input for output format, hence default is used: json"
 fi
 
-printf "$KEY_ID\n$KEY\n$REGION_NAME\n$OUTPUT_FORMAT" | aws configure --profile sandbox_user
+printf "$KEY_ID\n$KEY\n$REGION_NAME\n$OUTPUT_FORMAT" | aws configure --profile "$PROFILE_NAME"
 
 echo ""
-echo "setting env var AWS_PROFILE to sandbox_user"
-export AWS_PROFILE=sandbox_user
+echo "setting env var AWS_PROFILE to $PROFILE_NAME"
+export AWS_PROFILE=$PROFILE_NAME
 
-echo "[complete] setting up aws profile for sandbox_user is complete!"
+echo "[complete] setting up aws profile for $PROFILE_NAME is complete!"
 
 
 
