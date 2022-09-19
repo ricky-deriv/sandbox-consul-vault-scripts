3d61115b977a1edd564785883a985d9581af508d
ricky-deriv <ricky.thain@deriv.com>
2022-09-19 13:45:21 +0800

    script to create 5 instances

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### Index
 - configure aws cli to aws sandbox: setup_aws_cli.sh
 - generate if not exist a key-pair and upload to aws key pairs: import_key_pair.sh
+- create security groupn and rule to allow inbound ssh access from anywhere and create 5 ec2 instances: create_instances.sh
 
 ## Setting up AWS CLI for the sandbox
 file: setup_aws_cli.sh
@@ -21,3 +22,10 @@ Command:
 ```
 bash import_key_pair.sh
 ```
+
+## Create 5 EC2 instances
+file: create_instances.sh
+- create security group and a rule to allow inbound ssh from anywhere.
+- create five EC2 instances 
+- returns a list of EC2 instances' id.
+  
---
+++ create_instances.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "creating ec2 instances..."
+
+# set aws profile env variable
+export AWS_PROFILE=$(cat profile_name)
+
+# check profile in use
+user_exist=$(aws sts get-caller-identity | jq .Arn | grep -i cloud_user)
+
+if [[ -z "$user_exist" ]]; then
+    echo "username does not match"
+    exit
+fi
+
+KEY_PAIR_NAME="sandbox-key"
+INSTANCE_IDS=()
+
+echo "creating default security group for instances..."
+aws ec2 create-security-group --group-name default-security --description "default security group for instances"
+
+echo "creating allow inbound ssh access from anywhere..."
+aws ec2 authorize-security-group-ingress \
+    --group-name default-security \
+    --protocol tcp \
+    --port 22 \
+    --cidr 0.0.0.0/0
+
+for i in {1..5}
+do 
+    ec2_info=$(aws ec2 run-instances \
+	--image-id ami-09a41e26df464c548 \
+	--instance-type t2.micro \
+    --security-groups default-security \
+	--key-name $KEY_PAIR_NAME | jq .Instances[].InstanceId)
+
+    INSTANCE_IDS+=( $ec2_info )
+done
+
+echo "${INSTANCE_IDS[*]}"
+
+
+echo "[complete] creation of ec2 instances is complete!"
