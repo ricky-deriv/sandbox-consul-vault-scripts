09887502becf7f6bce8f460696af073332a03eee
ricky-deriv <ricky.thain@deriv.com>
2022-09-21 16:15:58 +0800

    change input from receiving dns ipv4 names to ec2 ids

--- bulk_install_for_consul.sh
+++ bulk_install_for_consul.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# set aws profile env variable
+export AWS_PROFILE=$(cat profile_name)
+
+# check profile in use
+user_exist=$(aws sts get-caller-identity | jq .Arn | grep -i cloud_user)
+
+if [[ -z "$user_exist" ]]; then
+    echo "username does not match"
+    exit
+fi
+
+KEY_PAIR_NAME="sandbox-key"
+
 echo "running installation script in instances..."
 
-instances=($@)
+instances_ids=($@)
 
-for instance in "${instances[@]}";
+for id in "${instances_ids[@]}";
 do 
-    echo "installing for $instance..."
-    ssh -i "~/.ssh/sandbox-key" ec2-user@$instance 'bash -s' < install_for_consul.sh
+    dns_name=$(aws ec2 describe-instances --instance-ids $id --query 'Reservations[].Instances[].PublicDnsName' | jq -r .[])
+    echo "installing for $id..."
+    ssh -i "~/.ssh/sandbox-key" ec2-user@$dns_name 'bash -s' < install_for_consul.sh
 done
 
 echo "[complete] bulk install complete!"
